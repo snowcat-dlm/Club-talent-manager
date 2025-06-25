3b9bb0bc05555deda6e27d38310556f500e658a6
MasamiKoibuchi <26708399+masami-butchy@users.noreply.github.com>
2025-06-25 11:47:13 +0000

    players appをsettings.pyへ登録

--- talentManager/config/settings.py
+++ talentManager/config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'players.apps.PlayersConfig',
     'django_bootstrap5',
     #'crispy-bootstrap5',
     #'django-crispy-forms',
