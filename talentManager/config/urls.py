9a9279457cae173c35d57687bcc6d487d31fa37f
MasamiKoibuchi <26708399+masami-butchy@users.noreply.github.com>
2025-06-25 11:55:04 +0000

    players-appのURL設定取り込み設定、STATIC_URL、MEDIA_URL設定追加

--- talentManager/config/urls.py
+++ talentManager/config/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings  #settings.pyをインポート
+from django.conf.urls.static import static  #追加（静的ファイル）
+#from .views import TopPage  #追加（トップページ）
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path(settings.ADMIN_URL, admin.site.urls),
+    path('', include('players.urls')),
+    #path('', TopPage.as_view(), name='toppage'),        #追加（トップページ）
+    #path('players/', include('players.urls')),          #追加（アプリ）
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+ #追加（静的ファイル）
